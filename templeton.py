ecf074a63bbf35088f9e8290915c79b3e968d814
doubleO8 <wb008@hdm-stuttgart.de>
2017-11-15 18:59:57 +0100

    template based pack/CONTROL/control

--- templeton.py
+++ templeton.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import os
+import beppo
 from jinja2 import Environment, PackageLoader
 
+if __name__ == '__main__':
+    env = Environment(loader=PackageLoader('beppo', 'templates'))
+    template = env.get_template('control')
 
-env = Environment(loader=PackageLoader('beppo', 'templates'))
-template = env.get_template('control')
-
-version_meta = {
-    "upstream_version": "1.2.3"
-}
-template_content = template.render(**version_meta)
-with open("pack/CONTROL/control", "wb") as target:
-    target.write(template_content)
+    version_meta = {
+        "upstream_version": beppo.__version__,
+        "epoch": 1
+    }
+    os.makedirs("pack/CONTROL")
+    template_content = template.render(**version_meta)
+    with open("pack/CONTROL/control", "wb") as target:
+        target.write(template_content)
